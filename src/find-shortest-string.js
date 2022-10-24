30741306fbe2740003050074ee43c7a10d95e3cf
Didier Osindero <didierosindero@gmail.com>
2022-10-24 14:30:00 +0100

    Debugged findShortestString function

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -14,7 +14,7 @@ function findShortestString(stringArr) {
       shortestString = str;
     }
   }
-  return shortestString.length;
+  return shortestString;
 }
 
 //Make this function accessible to test file
--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -2,7 +2,7 @@ const findShortestString = require("./find-shortest-string");
 
 test("findLongestString finds the longest string in an array", () => {
   expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
-  expect(findShortestString(["why", "hello", "to", "you"])).toBe("hello");
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
 });
 
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
